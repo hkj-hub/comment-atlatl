1579da3bc532f6323a3171d8510c8b7f016de761
hkj-hub <hisa.kato.job+github@gmail.com>
2023-07-04 21:57:09 +0900

    fix genrate token

--- src/domain/skyway/constants.ts
+++ src/domain/skyway/constants.ts
@@ -1,5 +1,3 @@
-export const applicationToken = process.env.NEXT_PUBLIC_APPLICATION_TOKEN;
-export const secretToken = process.env.NEXT_PUBLIC_SECRET_TOKEN;
 const params = global.window == null ? undefined : new URLSearchParams(window.location.search);
 export const roomName = params?.get('room') ?? 'lt';
 export const authToken = params?.get('token') ?? '';
--- src/domain/skyway/token.ts
+++ src/domain/skyway/token.ts
@@ -1,50 +1,7 @@
-import { applicationToken, authToken, secretToken } from './constants';
+import { authToken } from './constants';
 
 export const getToken = () => {
   if (authToken) return authToken;
   if (!window) return '';
-  const { SkyWayAuthToken, uuidV4, nowInSec } = window.skyway_room;
-  const token = new SkyWayAuthToken({
-    jti: uuidV4(),
-    iat: nowInSec(),
-    exp: nowInSec() + 60 * 60 * 24,
-    scope: {
-      app: {
-        id: `${applicationToken}`,
-        turn: true,
-        actions: ['read'],
-        channels: [
-          {
-            id: '*',
-            name: '*',
-            actions: ['write'],
-            members: [
-              {
-                id: '*',
-                name: '*',
-                actions: ['write'],
-                publication: {
-                  actions: ['write'],
-                },
-                subscription: {
-                  actions: ['write'],
-                },
-              },
-            ],
-            sfuBots: [
-              {
-                actions: ['write'],
-                forwardings: [
-                  {
-                    actions: ['write'],
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    },
-  }).encode(`${secretToken}`);
-  return token;
+  return '';
 };
