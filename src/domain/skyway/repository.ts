6eb83484fe82b046189ffddee9d50ae73d6d739e
hkj-hub <hisa.kato.job+github@gmail.com>
2023-07-04 20:32:31 +0900

    fix error nothing p2p

--- src/domain/skyway/repository.ts
+++ src/domain/skyway/repository.ts
@@ -1,36 +1,36 @@
-// import {
-//   LocalDataStream,
-//   LocalP2PRoomMember,
-//   RemoteDataStream,
-//   RoomPublication,
-//   SkyWayStreamFactory,
-// } from '@skyway-sdk/room';
+import {
+  LocalDataStream,
+  LocalP2PRoomMember,
+  RemoteDataStream,
+  RoomPublication,
+  SkyWayStreamFactory,
+} from '@skyway-sdk/room';
 
-// let stream: null | LocalDataStream = null;
+let stream: null | LocalDataStream = null;
 
-// export const getDataStream = async () => {
-//   if (stream) return stream;
-//   stream = await SkyWayStreamFactory.createDataStreama();
-//   return stream;
-// };
-// export const sendMessage = async (message: string) => {
-//   const dataStream = await getDataStream();
-//   console.log('send message', message);
-//   dataStream.write(message);
-// };
+export const getDataStream = async () => {
+  if (stream) return stream;
+  stream = await SkyWayStreamFactory.createDataStream();
+  return stream;
+};
+export const sendMessage = async (message: string) => {
+  const dataStream = await getDataStream();
+  console.log('send message', message);
+  dataStream.write(message);
+};
 
-// export const subscribeAndAttach = async (
-//   publication: RoomPublication,
-//   me: LocalP2PRoomMember,
-//   handler: (data: string) => void,
-// ) => {
-//   if (publication.publisher.id === me.id) return;
-//   const { stream } = await me.subscribe(publication.id);
-//   const dataStream = stream as RemoteDataStream;
-//   console.log('publication.publisher.id', publication.publisher.id);
-//   dataStream.onData.add((data) => {
-//     console.log('receive data', data);
-//     console.log('publisher.id', publication.publisher.id);
-//     handler(data.toString());
-//   });
-// };
+export const subscribeAndAttach = async (
+  publication: RoomPublication,
+  me: LocalP2PRoomMember,
+  handler: (data: string) => void,
+) => {
+  if (publication.publisher.id === me.id) return;
+  const { stream } = await me.subscribe(publication.id);
+  const dataStream = stream as RemoteDataStream;
+  console.log('publication.publisher.id', publication.publisher.id);
+  dataStream.onData.add((data) => {
+    console.log('receive data', data);
+    console.log('publisher.id', publication.publisher.id);
+    handler(data.toString());
+  });
+};
--- src/store/slices/p2pSlice.ts
+++ src/store/slices/p2pSlice.ts
@@ -1,42 +1,43 @@
-// import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-// import { AppDispatch } from '../store';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { AppDispatch } from '../store';
 // import { joinRoom } from '../../domain/skyway/room';
 // import { sendMessage } from '../../domain/skyway/repository';
-// import { getForce } from '../../domain/simulator/force';
-// import { simulator } from '../../domain/simulator';
+import { getForce } from '../../domain/simulator/force';
+import { simulator } from '../../domain/simulator';
 
-// export const joinP2PRoomAction = createAsyncThunk<string | null, void, { dispatch: AppDispatch }>(
-//   'joinP2PRoomAction',
-//   async (_req, thunkAPI) => {
-//     try {
-//       const id = await joinRoom((message) => {
-//         const force = getForce(message);
-//         simulator.addText({ text: message, position: { x: 200, y: 200 }, force });
-//       });
-//       return id;
-//     } catch (error: any) {
-//       return thunkAPI.rejectWithValue({ error: error.message });
-//     }
-//   },
-// );
-// const initialState = {
-//   peerId: '',
-// };
+export const joinP2PRoomAction = createAsyncThunk<string | null, void, { dispatch: AppDispatch }>(
+  'joinP2PRoomAction',
+  async (_req, thunkAPI) => {
+    try {
+      // const id = await joinRoom((message) => {
+      //   const force = getForce(message);
+      //   simulator.addText({ text: message, position: { x: 200, y: 200 }, force });
+      // });
+      const id = 'test';
+      return id;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue({ error: error.message });
+    }
+  },
+);
+const initialState = {
+  peerId: '',
+};
 
-// export const p2pSlice = createSlice({
-//   name: 'p2pSlice',
-//   initialState,
-//   reducers: {},
-//   extraReducers: (builder) => {
-//     builder.addCase(joinP2PRoomAction.fulfilled, (state, action) => {
-//       if (action.payload) state.peerId = action.payload;
-//     });
-//   },
-// });
+export const p2pSlice = createSlice({
+  name: 'p2pSlice',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(joinP2PRoomAction.fulfilled, (state, action) => {
+      if (action.payload) state.peerId = action.payload;
+    });
+  },
+});
 
-// export const sendMessageAction = createAsyncThunk<void, string>(
-//   'sendMessageAction',
-//   async (req) => {
-//     sendMessage(req);
-//   },
-// );
+export const sendMessageAction = createAsyncThunk<void, string>(
+  'sendMessageAction',
+  async (req) => {
+    // sendMessage(req);
+  },
+);
