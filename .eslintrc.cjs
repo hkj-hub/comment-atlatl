1c5dfc71857f30f6fa123694eff92990c43ed330
hkj-hub <hisa.kato.job+github@gmail.com>
2023-07-06 00:00:08 +0900

    add storybook init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: ['next/core-web-vitals', 'prettier'],
+  extends: ['next/core-web-vitals', 'plugin:storybook/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: [],
---
+++ docs/astro/src/pages/ja/memo/storybook.md
@@ -0,0 +1,2 @@
+[Spindle](https://spindle.ameba.design/principles/)
+[Storybook を Github.io にサブディレクトリを切って公開して MSW を導入したメモ](https://qiita.com/hibohiboo/items/9635105f6d2e909c6115)
