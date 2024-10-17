b53cfa726f89fbc6510ec25eabc793b2c557d9d3
hibohiboo <hibohiboo66+github@gmail.com>
2024-10-18 02:18:53 +0900

    fix error

--- next.config.js
+++ next.config.js
@@ -30,6 +30,10 @@ const nextConfig = {
             key: 'Cross-Origin-Embedder-Policy',
             value: 'require-corp',
           },
+          {
+            key: 'Cross-Origin-Resource-Policy',
+            value: 'cross-origin',
+          },
         ],
       },
     ];
