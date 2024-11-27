aead514e2e1cb2f3deeb0afc0818aab55470375b
hibohiboo <hibohiboo66+github@gmail.com>
2024-11-27 21:26:50 +0900

    adf summary #49

--- docs/code-maat/summary-csv.sh
+++ docs/code-maat/summary-csv.sh
@@ -2,9 +2,10 @@
 
 bin_dir=$(cd $(dirname $0) && pwd)
 maat_dir=$(cd $bin_dir/../../../tmp/code-maat && pwd)
+root_dir=$(cd $bin_dir/../.. && pwd)
 
 cd $maat_dir
 
 docker run -v $bin_dir:/data -it code-maat-app -l /data/logfile.log -c git2 > $bin_dir/logfile.csv
 docker run -v $bin_dir:/data -it code-maat-app -l /data/logfile.log -c git2 -a revisions > $bin_dir/revisions.csv
-
+docker run --rm -v $root_dir:/tmp aldanial/cloc ./src --unix --by-file --csv --quiet --report-file=./docs/code-maat/complexity.csv
