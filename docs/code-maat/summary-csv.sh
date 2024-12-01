8269f8a3079485d8092d6a7d1127904fad9f6bee
hibohiboo <hibohiboo66+github@gmail.com>
2024-12-01 21:45:29 +0900

    fix summary

--- docs/code-maat/summary-csv.sh
+++ docs/code-maat/summary-csv.sh
@@ -6,6 +6,6 @@ root_dir=$(cd $bin_dir/../.. && pwd)
 
 cd $maat_dir
 
-docker run -v $bin_dir:/data -it code-maat-app -l /data/logfile.log -c git2 > $bin_dir/logfile.csv
+# docker run -v $bin_dir:/data -it code-maat-app -l /data/logfile.log -c git2 > $bin_dir/logfile.csv
 docker run -v $bin_dir:/data -it code-maat-app -l /data/logfile.log -c git2 -a revisions > $bin_dir/revisions.csv
-docker run --rm -v $root_dir:/tmp aldanial/cloc ./ --unix --by-file --csv --quiet --vcs=git --exclude-dir=docs,.vscode,.github --not-match-f=package-lock\.json --report-file=./docs/code-maat/complexity.csv
+docker run --rm -v $root_dir:/tmp aldanial/cloc . --unix --by-file --csv --quiet --timeout 10 --vcs=git --exclude-dir=docs,.vscode,.github --not-match-f=package-lock\.json --report-file=./docs/code-maat/complexity.csv
