d1d66ee2ce78e1528b22d71757ac3d49b21963be
hibohiboo <hibohiboo66+github@gmail.com>
2025-03-12 22:06:32 +0900

    fix: update package dependencies and improve logging date filter

--- docs/code-maat/create_log.sh
+++ docs/code-maat/create_log.sh
@@ -2,5 +2,5 @@
 
 # https://github.com/adamtornhill/code-maat
 # ※ afterの合理的なデフォルトの日付は1年前 ... Code as a crime sience - P24
-git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames --after=2022-01-01 > logfile.log
+git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames --after=2023-01-01 > logfile.log
 
